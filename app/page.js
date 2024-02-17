92dd54300db10444c6c0bbb2d06835cc7977cb53
r2hu1 <newasram86@gmail.com>
2024-02-17 23:53:58 +0530

    final commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,115 @@
-import Image from "next/image";
+"use client";
+import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
+import { html } from '@codemirror/lang-html';
+import { css } from '@codemirror/lang-css';
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { useRef, useState } from 'react';
+import { vscodeDark, vscodeDarkInit } from '@uiw/codemirror-theme-vscode';
+import Footer from '@/components/Footer';
 
-export default function Home() {
+export default function Page() {
+  const darkTheme = {
+    background: "hsl(222.2 84% 4.9%)",
+    foreground: "#f1f5f9",
+    caret: "#f87171",
+    selection: "#f87171",
+    selectionMatch: "#f87171",
+    lineHighlight: "#374151",
+    gutterBackground: "hsl(222.2 84% 4.9%)",
+    gutterForeground: "#f1f5f9",
+  }
+
+  const [htmlValue, setHtmlValue] = useState(`<div class="demo">
+  <h1>Hello World</h1>
+</div>`);
+  const [cssValue, setCssValue] = useState(`.demo{
+    background:#525fe1;
+    color:#000;
+    font-family:system-ui,-apple-system;
+    height:100px;
+    padding:0px 40px;
+    border-radius:10px;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+  }`);
+  const [jsValue, setJsValue] = useState(`console.log("Hello World")`);
+
+  const iframeRef = useRef();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <ResizablePanelGroup direction="vertical" className="absolute h-full w-full top-0 left-0 right-0">
+      <ResizablePanel defaultSize={70}>
+        <ResizablePanelGroup direction="horizontal">
+          <ResizablePanel defaultSize={32}>
+            <CodeMirror className='codeScrollbar' maxHeight='100vh' minHeight='400px' height='100%' theme={vscodeDarkInit({
+              settings: {
+                background: "hsl(222.2 84% 4.9%)",
+                foreground: "#f1f5f9",
+                caret: "#f87171",
+                selection: "#f87171",
+                selectionMatch: "#f87171",
+                lineHighlight: "hsl(0, 0%, 100% / .1)",
+                gutterBackground: "hsl(222.2 84% 4.9%)",
+                gutterForeground: "#f1f5f9",
+              }
+            })} value={htmlValue} onChange={(val, viewUpdate) => { setHtmlValue(val) }} extensions={[html()]} />
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel defaultSize={32}>
+            <CodeMirror className='codeScrollbar' maxHeight='100vh' minHeight='400px' height='100%' theme={vscodeDarkInit({
+              settings: {
+                background: "hsl(222.2 84% 4.9%)",
+                foreground: "#f1f5f9",
+                caret: "#f87171",
+                selection: "#f87171",
+                selectionMatch: "#f87171",
+                lineHighlight: "hsl(0, 0%, 100% / .1)",
+                gutterBackground: "hsl(222.2 84% 4.9%)",
+                gutterForeground: "#f1f5f9",
+              }
+            })} value={cssValue} onChange={(val, viewUpdate) => { setCssValue(val) }} extensions={[css()]} />
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel defaultSize={32}>
+            <CodeMirror className='codeScrollbar' maxHeight='100vh' minHeight='400px' height='100%' theme={vscodeDarkInit({
+              settings: {
+                background: "hsl(222.2 84% 4.9%)",
+                foreground: "#f1f5f9",
+                caret: "#f87171",
+                selection: "#f87171",
+                selectionMatch: "#f87171",
+                lineHighlight: "hsl(0, 0%, 100% / .1)",
+                gutterBackground: "hsl(222.2 84% 4.9%)",
+                gutterForeground: "#f1f5f9",
+              }
+            })} value={jsValue} onChange={(val, viewUpdate) => { setJsValue(val) }} extensions={[javascript({ jsx: true })]} />
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </ResizablePanel>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <ResizableHandle withHandle />
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
-}
+      <ResizablePanel defaultSize={30} className='p-0 m-0'>
+        <iframe ref={iframeRef} className={'h-full w-full p-0 m-0'} srcDoc={`
+          <!DOCTYPE html>
+          <html lang="en">
+            <head>
+            </head>
+            <style>${cssValue}</style>
+            <body>
+            <div>${htmlValue}</div>
+            <script>${jsValue}</script>
+            </body>
+          </html>
+          `} />
+      </ResizablePanel>
+      <Footer onClear={() => { setHtmlValue("<!-- Write HTML -->"); setCssValue("/* Write CSS */"); setJsValue("// Write JavaScript") }} />
+    </ResizablePanelGroup>
+  )
+};
