04f17deabf1ad0f62f754d1d3f26ebd35cc800eb
r2hu1 <hi@rahul.eu.org>
2024-09-11 11:50:02 +0530

    fix hooks and added sharable contexthook

--- app/page.js
+++ app/page.js
@@ -2,25 +2,30 @@
 import Editor from "@/components/Editor";
 import MobileEditor from "@/components/MobileEditor";
 import useMediaQuery from '../hooks/useMediaQuery';
-import { Loader2 } from "lucide-react";
-import { useState, useEffect, Fragment } from "react";
+import { Code2, Loader2 } from "lucide-react";
+import { useState, useEffect, Fragment, createContext } from "react";
 
+export const CodeContext = createContext(null);
 export default function Page() {
   const screenSize = useMediaQuery("768");
   const [preload, setPreload] = useState(true);
+  const [codes, setCodes] = useState(null);
+  const [html, setHtml] = useState("");
+  const [css, setCss] = useState("");
+  const [js, setJs] = useState("");
 
   useEffect(() => {
     setPreload(false);
   }, []);
 
   return (
-    <Fragment>
+    <CodeContext.Provider value={{ codes, setCodes, html, setHtml, css, setCss, js, setJs }}>
       {!screenSize ? <Editor /> : <MobileEditor />}
       {preload && (
         <div className="absolute h-full w-full z-50 bg-background flex items-center justify-center">
-          <Loader2 className="h-6 w-6 animate-spin" />
+          <Code2 className="h-6 w-6 animate-pulse" />
         </div>
       )}
-    </Fragment>
+    </CodeContext.Provider>
   )
 };
