0d3f07d4b48947b409e0fa34e37b706b487c8a39
r2hu1 <hi@rahul.eu.org>
2024-12-11 12:39:34 +0530

    added loading

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import MobileEditor from "@/components/MobileEditor";
 import useMediaQuery from '../hooks/useMediaQuery';
 import { Code2, Loader2 } from "lucide-react";
 import { useState, useEffect, Fragment, createContext } from "react";
+import { download } from "@/lib/download";
 
 export const CodeContext = createContext(null);
 export default function Page() {
@@ -16,16 +17,28 @@ export default function Page() {
 
   useEffect(() => {
     setPreload(false);
+    const preventDefaultSave = (e) => {
+      if (e.key === "s" && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+        e.preventDefault();
+      }
+    };
+    document.addEventListener("keydown", preventDefaultSave, false);
+    return () => {
+      document.removeEventListener("keydown", preventDefaultSave, false);
+    };
   }, []);
 
   return (
-    <CodeContext.Provider value={{ codes, setCodes, html, setHtml, css, setCss, js, setJs }}>
-      {!screenSize ? <Editor /> : <MobileEditor />}
+    <>
       {preload && (
-        <div className="absolute h-full w-full z-50 bg-background flex items-center justify-center">
-          <Code2 className="h-6 w-6 animate-pulse" />
+        <div className="absolute h-full w-full z-[99999] bg-background flex items-center justify-center">
+          <Loader2 className="h-6 w-6 animate-spin" />
         </div>
       )}
-    </CodeContext.Provider>
+      < CodeContext.Provider value={{ codes, setCodes, html, setHtml, css, setCss, js, setJs }
+      }>
+        {!screenSize ? <Editor /> : <MobileEditor />}
+      </CodeContext.Provider >
+    </>
   )
 };
