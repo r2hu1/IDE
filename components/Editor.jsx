b1cd61bde8eeca847f9a908deb43b156a3710e68
r2hu1 <hi@rahul.eu.org>
2024-12-11 12:20:15 +0530

    chnged layout

--- components/Editor.jsx
+++ components/Editor.jsx
@@ -1,17 +1,18 @@
 "use client";
-import { useEffect, useCallback, memo, Fragment, useContext, useMemo } from 'react';
+import { useEffect, useCallback, memo, Fragment, useContext, useMemo, useState } from 'react';
 import { download } from '@/lib/download';
 import { loadLanguage } from "@uiw/codemirror-extensions-langs";
 import CodeMirror from '@uiw/react-codemirror';
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
 import Footer from '@/components/Footer';
 import { Button } from './ui/button';
-import { Save } from 'lucide-react';
+import { GitFork, Save, Star } from 'lucide-react';
 import { toast } from 'sonner';
 import { cmOptions } from '@/lib/cmOptions';
 import { ScrollArea, ScrollBar } from './ui/scroll-area';
 import { CodeContext } from '@/app/page';
 import { debounced } from '@/lib/debounced';
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 
 const codeTypes = ['html', 'css', 'javascript'];
 
@@ -20,6 +21,7 @@ const Editor = () => {
     const codeC = useContext(CodeContext);
     const codeJ = useContext(CodeContext);
     const values = useContext(CodeContext);
+    const [currTab, setCurrTab] = useState('html');
 
     const handleDownload = useCallback(() => {
         download({ src: values.codes });
@@ -44,43 +46,45 @@ const Editor = () => {
     }, [codeH.setHtml, codeC.setCss, codeJ.setJs]);
 
     const renderCodePanel = useCallback((type) => (
-        <ResizablePanel key={type} defaultSize={32}>
-            <ScrollArea className="h-full w-full">
-                <ScrollArea className="h-full w-full">
-                    <CodeMirror
-                        {...cmOptions}
-                        value={type === 'html' ? codeH.html : (type === 'css' ? codeC.css : codeJ.js)}
-                        placeholder={type.toUpperCase()}
-                        onChange={(val) => handleChange(val, type)}
-                        extensions={[loadLanguage(type)]}
-                    />
-                </ScrollArea>
-                <ScrollBar orientation="horizontal" />
-            </ScrollArea>
-        </ResizablePanel>
+        <CodeMirror
+            {...cmOptions}
+            // className='w-full h-[calc(100vh-57px)]'
+            value={type === 'html' ? codeH.html : (type === 'css' ? codeC.css : codeJ.js)}
+            placeholder={`Write your ${type.toUpperCase()}`}
+            onChange={(val) => handleChange(val, type)}
+            extensions={[loadLanguage(type)]}
+        />
     ), [codeH.html, codeC.css, codeJ.js, handleChange]);
 
     return (
-        <ResizablePanelGroup direction="vertical" className="absolute h-full w-full top-0 left-0 right-0">
-            <ResizablePanel defaultSize={60}>
-                <ResizablePanelGroup direction="horizontal">
-                    {renderCodePanel("html")}
-                    <ResizableHandle withHandle />
-                    {renderCodePanel("css")}
-                    <ResizableHandle withHandle />
-                    {renderCodePanel("javascript")}
-                </ResizablePanelGroup>
-            </ResizablePanel>
-
-            <ResizableHandle withHandle />
-
-            <ResizablePanel defaultSize={40} className='p-0 m-0'>
-                <iframe className={'h-full w-full p-0 m-0 bg-white'} srcDoc={values.codes} />
-            </ResizablePanel>
-            <Footer onClear={() => { codeH.setHtml(""); codeC.setCss(""); codeJ.setJs(""); values.setCodes(""); }} >
-                <Button size="icon" variant="secondary" onClick={handleDownload}><Save className="h-4 w-4" /></Button>
-            </Footer>
-        </ResizablePanelGroup>
+        <div>
+            <div className="w-full flex items-center justify-between p-2.5 border-b">
+                <ToggleGroup className="w-fit" size="sm" type="single" defaultValue={currTab} onValueChange={setCurrTab}>
+                    <ToggleGroupItem disabled={currTab === "html"} value="html">HTML</ToggleGroupItem>
+                    <ToggleGroupItem disabled={currTab === "css"} value="css">CSS</ToggleGroupItem>
+                    <ToggleGroupItem disabled={currTab == "javascript"} value="javascript">JavaScript</ToggleGroupItem>
+                </ToggleGroup>
+                <div className='flex gap-1'>
+                    <Button size="sm" onClick={() => { codeH.setHtml(""); codeC.setCss(""); codeJ.setJs(""); values.setCodes(""); }}>Clear</Button>
+                    <Button size="icon" variant="secondary" onClick={handleDownload}><Save className="h-4 w-4" /></Button>
+                </div>
+            </div>
+            <ResizablePanelGroup direction="horizontal">
+                <ResizablePanel minSize={20}>
+                    <ScrollArea className="h-[calc(100vh-57px)] w-full">
+                        <ScrollArea>
+                            {renderCodePanel(currTab)}
+                            <ScrollBar/>
+                        </ScrollArea>
+                        <ScrollBar orientation="vertical" />
+                    </ScrollArea>
+                </ResizablePanel>
+                <ResizableHandle withHandle />
+                <ResizablePanel minSize={20}>
+                    <iframe className={'h-[calc(100vh-57px)] w-full p-0 m-0 bg-white'} srcDoc={values.codes} />
+                </ResizablePanel>
+            </ResizablePanelGroup>
+        </div>
     );
 };
 
